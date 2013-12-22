a61f047f4b78709b069e66166774159116061b4a
Adam O'Grady <adam.ogrady@gmail.com>
2013-12-22 16:22:12 +0800

    Error handling, colour updating
    Added error handling to datagram sending. Also changed so it updates a bulb's full colour rather than just a full R, G or B of one bulb.

--- wiki-holiday.js
+++ wiki-holiday.js
@@ -1,21 +1,33 @@
-var WebSocket = require('ws');
-var ws = new WebSocket('ws://wikimon.hatnote.com/en/');
+var WebSocket = require('ws'); //Require ws Websockets package
+var ws = new WebSocket('ws://wikimon.hatnote.com/en/'); //Open websocket to Wikipedia edit stream
 
-var globes = new Uint8Array(160);
+var globes = new Uint8Array(160); //Create array of globes
 
+//Clear all globes to black/off
 for (i = 0; i < 160; i++) {
 	globes[i] = 0;
 }
 
-
+//Upon receiving a message
 ws.on('message', function(data, flags) {
-	var dgram = require('dgram');
-	socket = dgram.createSocket('udp4');
+	var dgram = require('dgram'); //Require datagram package (built in to Node.js)
+	socket = dgram.createSocket('udp4'); //Create a datagram socket to send from
 	
-	globes[Math.round(Math.random() * 149) + 11] = 255;
+	light = Math.round(Math.random() * 49) * 3 + 10; //Choose a random light
+	//Set the RGB colour of that light
+	globes[light] = Math.round(Math.random() * 256);
+	globes[light + 1] = Math.round(Math.random() * 256);
+	globes[light + 2] = Math.round(Math.random() * 256);
+	//Clear two random lights to keep it looking like it's twinkling changing
+	globes[Math.round(Math.random() * 149) + 11] = 0;
 	globes[Math.round(Math.random() * 149) + 11] = 0;
 	
-	var message = new Buffer(globes);
+	var message = new Buffer(globes); //Create a datagram message from the array of globe values
 	
-	socket.send(message, 0, message.length, 9988, "192.168.0.115");
+	//Send the datagram containing globe values
+	socket.send(message, 0, message.length, 9988, "192.168.0.115", function(err, bytes) { 
+		if (err) { //If datagram sending errors...
+			console.log(err); //Log error to console
+		}
+	});
 });
